6fa274e3fe979ff5ecc9b0e7dbe044358b44ba03
ThinkAgent2 <thinkagent2@proton.me>
2026-02-17 17:02:27 +0000

    fix: remove default badge in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -114,7 +114,7 @@ export function Header() {
                   </AvatarFallback>
                   {user.featured_badge_id && (
                     <AvatarBadge className="text-xs">
-                      {userBadgesMap.get(user.featured_badge_id) || '🏅'}
+                      {userBadgesMap.get(user.featured_badge_id)}
                     </AvatarBadge>
                   )}
                 </Avatar>
